728bb0a1e9b8281f040871a32f974688123bcaac
Abdulrhman Soliman <abdo.soliman156@gmail.com>
2025-01-23 18:48:07 +0200

    Revert "improvements"

--- src/Weather.js
+++ src/Weather.js
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { ReactComponent as Humidity } from "./humidity.svg";
 import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 import moment from "moment";
 import "moment/min/locales.min";
